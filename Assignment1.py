bd2aac8ed2fa4b895f90413cf3809953ff67bccf
Deepika verma <deepikavermav049@gmail.com>
2026-03-16 23:36:43 +0530

    Add files via upload
    # Q1 Write a python program that points the following text exactly as it appears:
    # Q2. For a buisness create 3 variables to store - name , age and city.then print a sentence that uses these Variables.

---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+#Assignment
+print("hello world")
+print('Hello world')
+print("you're a good man")
+print('''you're a "good "person''')
+
+# Q1 Write a python program that points the following text exactly as it appears:
+print("python is fun.")
+print("quotes and 'single quotoes'can be tricky .")
+print("\"quotes\" and 'single quotoes'can be tricky .")
+
+print("Python is fun\n\"quotes\" and 'single quotoes'can be tricky .")
+
+# Q2. For a buisness create 3 variables to store - name , age and city.then print a sentence that uses these Variables.
+name = "Deepika"
+age = 25 
+city = "Gwalior"
+print ("My name is", name, "from", city ,"&, I,m",age )
+
+print (f"My name is{name}from {city} & I'm {age} ")
